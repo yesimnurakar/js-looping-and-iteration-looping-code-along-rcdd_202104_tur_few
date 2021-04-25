f25ee9b3e8ee7a4bd0ebe354eb65f9e73d215e5c
Yesim nur Akar <yesimnurakar@gmail.com>
2021-04-25 20:59:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event= "surprise";
+function writeCards(names, event) {
+let paragraph = []
+for (let i = 0; i < names.length; i++) {
+paragraph.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+ }
+return paragraph;
+}
+
+function countDown() {
+  let numero=10;
+  while (numero>=0) {
+    console.log(numero--);
+  }
+}
