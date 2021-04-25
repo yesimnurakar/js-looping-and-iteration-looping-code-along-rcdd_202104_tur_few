43abf1e61a6afbcb1ecfb8b96d482b88c79fe951
Yesim nur Akar <yesimnurakar@gmail.com>
2021-04-25 20:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const names = ['Lisa', 'Kaitlin', 'Jan'];
 const event= "surprise";
 function writeCards(names, event) {
-let paragraph = [] 
+let paragraph = []
 for (let i = 0; i < names.length; i++) {
 paragraph.push(`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
  }
